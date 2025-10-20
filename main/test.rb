106f26ec0929563fb6ad219ecd3f1538c03a51f0
Klejdi Skenderaj <klejdi@Klejdis-MacBook-Air.local>
2025-10-21 01:47:34 +0200

    Ready finished frontend most of it now waiting to combine front and backend for final resultsgit add .

--- main/test.rb
+++
@@ -1,7 +0,0 @@
-require "oauth2"
-UID = "Your application uid"
-SECRET = "Your secret token"
-# Create the client with your credentials
-client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
-# Get an access token
-token = client.client_credentials.get_token
